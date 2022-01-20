4fa120aa840c2d7e2cd5ca9d063526c3f271d5c7
Hayden Mankin <mankin.5@wright.edu>
2022-01-19 23:01:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,32 @@
  * Purpose: 
  */
 
-public class Main {
-  public static String Hello() {
-    return "World";
+public class Main {  
+  public static int getSurfaceArea(int sideLength) {
+    /*
+     * Write code to calculate the surface area, given sideLength is already declared as the correct value.
+     * replace the `0` below with the value of the surface area.
+     */
+    
+    return 0;
+  }
+  
+  public static int getVolume(int sideLength) {
+    /*
+     * Write code to calculate the surface area, given sideLength is already declared as the correct value.
+     * replace the `0` below with the value for volume.
+     */
+    
+    return 0;
+  }
+  
+  public static void main(String[] args) {
+    /*
+     * Write your own tests here if necessary. When you run your program, the above methods will be tested, and then this method will be run.
+     * I expect you to write your own tests as well for full credit on this.
+     */
+    
+    // you can call the above methods like this:
+    int volume = getVolume(5);
   }
 }
